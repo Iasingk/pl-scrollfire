46838d5fbe24b3a4a01111f1b44d5ba6770e7ae6
sexar <iconfidence@gmail.com>
2017-10-26 21:52:04 -0500

    Initial commit

---
+++ scripts/ts/Scrollfire.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by cesarmejia on 26/10/2017.
+ */
+module pl {
+
+    export class ScrollFire {
+
+        /**
+         * Create a Scroll Fire instance.
+         * @param {HTMLElement} items
+         * @param {Object} settings
+         */
+        constructor(items: HTMLElement, settings: Object) {
+
+        }
+
+    }
+
+}
